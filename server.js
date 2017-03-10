a7f7af963a88facb7242f2e86efd47d8c6cde5e0
“mohamedfawzy96” <mohamedfawzydes@gmail.com>
2017-03-10 22:17:04 +0200

    FOR DEPLOYING added procfile

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
 	app = express(),
-	port = 8080,
+	port = process.env.PORT || 8000
 	bodyParser = require('body-parser'),
 	//ejsLayouts = require("express-ejs-layouts");
 	mongoose = require('mongoose');
